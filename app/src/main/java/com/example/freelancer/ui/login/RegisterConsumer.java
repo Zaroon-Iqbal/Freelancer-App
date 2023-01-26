64f88f264152b2f737ef5eca941e28a4d25bbd24
Zaroon-Iqbal <zaroon123@live.com>
2023-01-25 23:33:12 -0800

    Consumer Registration UI

---
+++ app/src/main/java/com/example/freelancer/ui/login/RegisterConsumer.java
@@ -0,0 +1,19 @@
+package com.example.freelancer.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.freelancer.R;
+
+
+public class RegisterConsumer extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_consumer);
+    }
+}
