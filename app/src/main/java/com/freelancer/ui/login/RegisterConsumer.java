8d360e2e0d61d728ffc76759c1b54ed9249c8483
Zaroon-Iqbal <zaroon123@live.com>
2023-01-26 00:33:05 -0800

    Added the RegisterConsumer activity to the AndroidManifest file to make it correctly launch

--- app/src/main/java/com/freelancer/ui/login/RegisterConsumer.java
+++ app/src/main/java/com/freelancer/ui/login/RegisterConsumer.java
@@ -6,7 +6,9 @@ package com.example.freelancer.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.freelancer.R;
 
@@ -18,7 +20,11 @@ public class RegisterConsumer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, "Bye", Toast.LENGTH_SHORT);
+        toast.show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_consumer);
+        toast.show();
     }
 }
