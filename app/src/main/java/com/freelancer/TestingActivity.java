6afa5ffbbca770fa47d972ec91b5af4bf50c47bc
Zaroon-Iqbal <zaroon123@live.com>
2023-02-07 18:24:32 -0800

    git not recognizing changes?

--- app/src/main/java/com/freelancer/TestingActivity.java
+++ app/src/main/java/com/freelancer/TestingActivity.java
@@ -10,5 +10,6 @@ public class TestingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testing);
+        //git test
     }
 }
