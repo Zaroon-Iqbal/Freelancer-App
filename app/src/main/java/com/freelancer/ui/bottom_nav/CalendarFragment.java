0e27da19d08a007cb0b83a287f89ea6b55e0b9cf
EdKuoch <edward_kuoch@yahoo.com>
2023-02-16 17:14:29 -0800

    Fixed Bottom Nav Calendar Button
    Connected the correct activity to the the calendar button on the nav bar.

--- app/src/main/java/com/freelancer/ui/bottom_nav/CalendarFragment.java
+++ app/src/main/java/com/freelancer/ui/bottom_nav/CalendarFragment.java
@@ -15,6 +15,6 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_calendar, container, false);
+        return inflater.inflate(R.layout.activity_calendar, container, false);
     }
 }
