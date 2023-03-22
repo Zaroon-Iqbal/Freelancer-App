35d5d9902eb721884708370cf5841e5fa3b0811b
EdKuoch <edward_kuoch@yahoo.com>
2023-03-21 21:45:09 -0700

    Created checklist
    Includes the UI and the class file. Using the message bottom nav icon.

--- app/src/main/java/com/freelancer/ui/bottom_nav/MessageFragment.java
+++ app/src/main/java/com/freelancer/ui/bottom_nav/MessageFragment.java
@@ -15,6 +15,6 @@ public class MessageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.message_placeholder, container, false);
+        return inflater.inflate(R.layout.activity_checklist, container, false);
     }
 }
