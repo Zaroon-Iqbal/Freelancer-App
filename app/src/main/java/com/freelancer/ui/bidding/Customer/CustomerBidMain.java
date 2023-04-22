4a868076301ebc63b5a4d83f00da1f2af06e7c0e
EdKuoch <edward_kuoch@yahoo.com>
2023-04-21 17:49:15 -0700

    Consumer Bidding U.I.
    created the u.i. for consumer bidding.

---
+++ app/src/main/java/com/freelancer/ui/bidding/Customer/CustomerBidMain.java
@@ -0,0 +1,16 @@
+package com.freelancer.ui.bidding.Customer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.freelancer.R;
+
+public class CustomerBidMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bidding_consumer_main);
+    }
+}
