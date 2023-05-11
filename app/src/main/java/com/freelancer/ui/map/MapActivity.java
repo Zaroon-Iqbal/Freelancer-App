d278cbcc20abe75f4d5a45f71306d43352a59cb0
EdKuoch <edward_kuoch@yahoo.com>
2023-05-10 22:34:02 -0700

    Added directions/polyline to the Map
    On the map, it creates a polyline that makes the calculated route visible

--- app/src/main/java/com/freelancer/ui/map/MapActivity.java
+++ app/src/main/java/com/freelancer/ui/map/MapActivity.java
@@ -1,13 +1,19 @@
 package com.freelancer.ui.map;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.freelancer.R;
@@ -18,9 +24,21 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 This is the activity responsible for accessing the Google Map API and displaying the current
@@ -32,11 +50,13 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     Location currentLocation;
     FusedLocationProviderClient fusedClient;
     private static final int REQUEST_CODE = 101;
+    private GeoApiContext mGeoApiContext = null;
+    private GoogleMap gMap;
     //When created it creates a fused client and determines permissions.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_fragment);
+        setContentView(R.layout.activity_map);
         map = findViewById(R.id.maps);
         fusedClient = LocationServices.getFusedLocationProviderClient(this);
         getLocation();
@@ -68,11 +88,19 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     //Provides the Longitude and Latitude coordinates for the map and creates a marker.
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
         LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("My Current Location");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-        googleMap.addMarker(markerOptions);
+        gMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        gMap.addMarker(markerOptions);
+        if(mGeoApiContext == null){
+            mGeoApiContext = new GeoApiContext.Builder().apiKey("AIzaSyCo0h09NsMI8Kmj6t-T6RCPRo6pMXfHLnQ").build();
+        }
+        LatLng test = new LatLng(37.44864, -122.14741);
+        MarkerOptions endpoint = new MarkerOptions().position(test).title("EndPoint");
+        gMap.addMarker(endpoint);
+        calculateDirections(test);
     }
 
     //Checks to see if the user grants permission to use their location.
@@ -85,4 +113,71 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
         }
     }
+
+    //Calculates the direction between two points
+    private void calculateDirections(LatLng latLng){
+        Log.d(TAG, "calculateDirections: calculating directions.");
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                latLng.latitude,
+                latLng.longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        //directions.alternatives(true);
+        directions.origin(
+                new com.google.maps.model.LatLng(
+                        currentLocation.getLatitude(),
+                        currentLocation.getLongitude()
+                )
+        );
+        Log.d(TAG, "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "onResult: routes: " + result.routes[0].toString());
+                Log.d(TAG, "onResult: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "onFailure: " + e.getMessage() );
+
+            }
+        });
+    }
+
+    //Draws a polyline between the two points
+    private void addPolylinesToMap(final DirectionsResult result){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "run: result routes: " + result.routes.length);
+
+                for(DirectionsRoute route: result.routes){
+                    Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                    List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                    List<LatLng> newDecodedPath = new ArrayList<>();
+
+                    // This loops through all the LatLng coordinates of ONE polyline.
+                    for(com.google.maps.model.LatLng latLng: decodedPath){
+
+//                        Log.d(TAG, "run: latlng: " + latLng.toString());
+
+                        newDecodedPath.add(new LatLng(
+                                latLng.lat,
+                                latLng.lng
+                        ));
+                    }
+                    Polyline polyline = gMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
+                    polyline.setColor(ContextCompat.getColor(MapActivity.this, R.color.md_theme_dark_inverseOnSurface));
+                    polyline.setClickable(false);
+
+                }
+            }
+        });
+    }
+
 }
